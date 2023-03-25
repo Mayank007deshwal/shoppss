62df803e53385d0b6dfa233aa382c349cca674ae
mayank007deshwal <mayank007deshwal@gmail.com>
2023-03-25 12:49:22 +0530

    add success_url, cancel_url and incremet sales_count without webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # resources :checkout, only: [:create]
   post "checkout/create", to: "checkout#create"
   post "webhooks", to: "webhooks#create"
+  get "success", to: "checkout#success"
+  get "cancel", to: "checkout#cancel"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
